1b43a1245107ffc99646524ec0c50362b6b9aa46
Arlston Fernandes <gpg@arlston.com>
2020-07-15 13:24:42 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in hmcts_common_platform.gemspec
+gemspec
+
+gem 'rake', '~> 12.0'
---
+++ lib/hmcts_common_platform.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'hmcts_common_platform/version'
+
+module HmctsCommonPlatform
+  class Error < StandardError; end
+  # Your code goes here...
+end
